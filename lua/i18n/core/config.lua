4cb8ea8a1e4a5f3a3af60b3db6a33df8ecea9fd3
zerochae <zerochae@kakao.com>
2025-03-09 18:30:11 +0900

    fix(#master): setup function

--- lua/i18n/core/config.lua
+++ lua/i18n/core/config.lua
@@ -5,14 +5,4 @@ M.config = {
   projects = {},
 }
 
--- 설정 함수
-M.setup = function(opts)
-  if opts and opts.projects then
-    if type(opts.projects) ~= "table" then
-      error "projects must be a table (e.g., { projectA = { locales_path = '...', languages = {...} } })"
-    end
-  end
-  M.config = vim.tbl_extend("force", M.config, opts or {})
-end
-
 return M
--- lua/i18n/init.lua
+++ lua/i18n/init.lua
@@ -1,14 +1,20 @@
 local M = {}
 
--- Core 모듈
 M.config = require "i18n.core.config"
 M.loader = require "i18n.core.loader"
 
--- Finder 모듈
 M.treesitter = require "i18n.finder.treesitter"
 M.telescope = require "i18n.finder.telescope"
 
--- Renderer 모듈
 M.virtual_text = require "i18n.renderer.virtual_text"
+
+M.setup = function(opts)
+  if opts and opts.projects then
+    if type(opts.projects) ~= "table" then
+      error "projects must be a table (e.g., { projectA = { locales_path = '...', languages = {...} } })"
+    end
+  end
+  M.config = vim.tbl_extend("force", M.config, opts or {})
+end
 
 return M
